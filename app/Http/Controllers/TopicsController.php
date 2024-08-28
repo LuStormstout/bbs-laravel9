ca12c5b42280d44a002823bd7fa5183465f3e9f4
Lu Stormstout <lustormstout@gmail.com>
2024-08-28 12:04:18 +0900

    回复列表

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -87,7 +87,7 @@ class TopicsController extends Controller
         $topic->user_id = Auth::id();
         $topic->save();
 
-        return redirect()->route('topics.show', $topic->id)->with('success', '帖子创建成功！');
+        return redirect()->to($topic->link())->with('success', '成功创建话题！');
     }
 
 
@@ -117,8 +117,7 @@ class TopicsController extends Controller
     {
         $this->authorize('update', $topic);
         $topic->update($request->all());
-
-        return redirect()->route('topics.show', $topic->id)->with('success', '更新成功！');
+        return redirect()->to($topic->link())->with('success', '更新成功！');
     }
 
     /**
